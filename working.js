c5bd1a29da91025b0fb4773d67a12ae035f61884
Vishal chauhan <vishalchauhan0195@gmail.com>
2026-08-27 21:34:50 +0530

    Add time and Data

--- working.js
+++ working.js
@@ -8,7 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
-
 (function () {
   emailjs.init({
     publicKey: "YafjgJ7Wdom3uWua4"
@@ -17,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.getElementById("contact-form").addEventListener("submit", function (event) {
   event.preventDefault();
-
   emailjs.sendForm(
     "service_0521iog",
     "template_hi0f6qu",
@@ -32,3 +30,42 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
     alert("Message failed to send.");
   });
 });
+
+// Wish funtion
+function getGreeting() {
+  const hour = new Date().getHours();
+  if (hour < 12) {
+    return "Good Morning";
+  } else if (hour < 17) {
+    return "Good Afternoon";
+  } else if (hour < 21) {
+    return "Good Evening";
+  } else {
+    return "Good Night";
+  }
+}
+//Date funtion
+function getFormattedDate() {
+  const today = new Date();
+  return today.toLocaleDateString('en-IN', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+}
+document.getElementById('greeting').innerHTML = `${getGreeting()}! <br> Welcome to my Web Profile`;
+document.getElementById('current-date').textContent = `Today's ${getFormattedDate()}`;
+
+//Time funtion
+function updateTime() {
+    const now = new Date();
+    document.getElementById("time").textContent =
+        now.toLocaleTimeString("en-IN", {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        });
+}
+updateTime();
+setInterval(updateTime, 1000);
